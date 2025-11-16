cb2b0c984802fc897a0543479ed99cfcf2219769
Daniel Kuroski <daniel.kuroski@gmail.com>
2025-11-16 10:38:07 +0100

    wip

---
+++ src/parse.c
@@ -0,0 +1,11 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "parse.h"
+
